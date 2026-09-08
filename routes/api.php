5b0c84c9ec9021e65edefa0ed1440df80e1df913
ItsMeFadz <mr954904@gmail.com>
2026-09-08 13:35:50 +0700

    delete:controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\LunasKreditSyncController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('lunas-kredit')->group(function () {
-    Route::post('send', [LunasKreditSyncController::class, 'send']);
-});
+// Route::prefix('lunas-kredit')->group(function () {
+//     Route::post('send', [LunasKreditSyncController::class, 'send']);
+// });
